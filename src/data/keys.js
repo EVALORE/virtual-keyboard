60de33048df7f41d075c0e30c6c40447c07fd26e
EVALORE <vladimireremia377@gmail.com>
2023-04-30 22:08:54 +0300

    feat: add keys list

---
+++ src/data/keys.js
@@ -0,0 +1,305 @@
+export default [
+  {
+    code: 'Backquote',
+    en: ['`', '~'],
+    ru: ['ё', 'Ё'],
+  },
+  {
+    code: 'Digit1',
+    en: ['1', '!'],
+    ru: ['1', '!'],
+  },
+  {
+    code: 'Digit2',
+    en: ['2', '@'],
+    ru: ['2', '"'],
+  },
+  {
+    code: 'Digit3',
+    en: ['3', '#'],
+    ru: ['3', '№'],
+  },
+  {
+    code: 'Digit4',
+    en: ['4', '$'],
+    ru: ['4', ';'],
+  },
+  {
+    code: 'Digit5',
+    en: ['5', '%'],
+    ru: ['5', '%'],
+  },
+  {
+    code: 'Digit6',
+    en: ['6', '^'],
+    ru: ['6', ':'],
+  },
+  {
+    code: 'Digit7',
+    en: ['7', '&'],
+    ru: ['7', '?'],
+  },
+  {
+    code: 'Digit8',
+    en: ['8', '*'],
+    ru: ['8', '*'],
+  },
+  {
+    code: 'Digit8',
+    en: ['9', '('],
+    ru: ['9', '('],
+  },
+  {
+    code: 'Digit0',
+    en: ['0', ')'],
+    ru: ['0', ')'],
+  },
+  {
+    code: 'Minus',
+    en: ['-', '_'],
+    ru: ['-', '_'],
+  },
+  {
+    code: 'Equal',
+    en: ['=', '+'],
+    ru: ['=', '+'],
+  },
+  {
+    code: 'Backspace',
+    value: 'Backspace',
+  },
+  {
+    code: 'Tab',
+    value: 'Tab',
+  },
+  {
+    code: 'KeyQ',
+    en: ['q', 'Q'],
+    ru: ['й', 'Й'],
+  },
+  {
+    code: 'KeyW',
+    en: ['w', 'W'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyE',
+    en: ['e', 'E'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyR',
+    en: ['r', 'R'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyT',
+    en: ['t', 'T'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyY',
+    en: ['y', 'Y'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyU',
+    en: ['u', 'U'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyI',
+    en: ['i', 'I'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyO',
+    en: ['o', 'O'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyP',
+    en: ['p', 'P'],
+    ru: ['', ''],
+  },
+  {
+    code: 'BracketLeft',
+    en: ['[', '{'],
+    ru: ['', ''],
+  },
+  {
+    code: 'BracketRight',
+    en: [']', '}'],
+    ru: ['', ''],
+  },
+  {
+    code: 'Backslash',
+    en: ['\\', '|'],
+    ru: ['', ''],
+  },
+  {
+    code: 'Delete',
+    value: 'Delete',
+  },
+  {
+    code: 'CapsLock',
+    value: 'CapsLock',
+  },
+  {
+    code: 'KeyA',
+    en: ['a', 'A'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyS',
+    en: ['s', 'S'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyD',
+    en: ['d', 'D'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyF',
+    en: ['f', 'F'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyG',
+    en: ['g', 'G'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyH',
+    en: ['h', 'H'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyJ',
+    en: ['j', 'J'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyK',
+    en: ['k', 'K'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyL',
+    en: ['l', 'L'],
+    ru: ['', ''],
+  },
+  {
+    code: 'Semicolon',
+    en: [';', ':'],
+    ru: ['', ''],
+  },
+  {
+    code: 'Quote',
+    en: ["'", '"'],
+    ru: ['', ''],
+  },
+  {
+    code: 'Enter',
+    value: 'Enter',
+  },
+  {
+    code: 'ShiftLeft',
+    value: 'Shift',
+  },
+  {
+    code: 'KeyZ',
+    en: ['z', 'Z'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyX',
+    en: ['x', 'X'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyC',
+    en: ['c', 'C'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyV',
+    en: ['v', 'V'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyB',
+    en: ['b', 'B'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyN',
+    en: ['n', 'N'],
+    ru: ['', ''],
+  },
+  {
+    code: 'KeyM',
+    en: ['m', 'M'],
+    ru: ['', ''],
+  },
+  {
+    code: 'Comma',
+    en: [',', '<'],
+    ru: ['', ''],
+  },
+  {
+    code: 'Period',
+    en: ['.', '>'],
+    ru: ['', ''],
+  },
+  {
+    code: 'Slash',
+    en: ['/', '?'],
+    ru: ['', ''],
+  },
+  {
+    code: 'ArrowUp',
+    value: '▲',
+  },
+  {
+    code: 'ShiftRight',
+    value: 'Shift',
+  },
+  {
+    code: 'ControlLeft',
+    value: 'Ctrl',
+  },
+  {
+    code: 'MetaLeft',
+    value: 'Win',
+  },
+  {
+    code: 'AltLeft',
+    value: 'Alt',
+  },
+  {
+    code: 'Space',
+    value: ' ',
+  },
+  {
+    code: 'AltRight',
+    value: 'Alt',
+  },
+  {
+    code: 'ControlRight',
+    value: 'Ctrl',
+  },
+  {
+    code: 'ArrowLeft',
+    value: '◄',
+  },
+  {
+    code: 'ArrowDown',
+    value: '▼',
+  },
+  {
+    code: 'ArrowRight',
+    value: '►',
+  },
+];
