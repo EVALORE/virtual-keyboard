fd086e4a367e180e9c38e7e9fc1b8897da5d322f
EVALORE <vladimireremia377@gmail.com>
2023-05-01 23:10:00 +0300

    feat: add type separation to key class

--- src/Classes/Key.js
+++ src/Classes/Key.js
@@ -6,13 +6,17 @@ class Key {
     if ('value' in keyValues) {
       this.value = keyValues.value;
       this.currentValue = this.value;
+      this.type = 'special';
     } else {
-      this.values = keyValues;
-      [this.currentValue] = this.values.en;
+      this.lang = keyValues;
+      this.values = this.lang.en;
+      [this.currentValue] = this.values;
+      this.type = 'regular';
     }
 
     this.key = button;
     this.key.textContent = this.currentValue;
+    this.action = null;
   }
 }
 
